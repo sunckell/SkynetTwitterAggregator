5c5c5cd83365768eeb8021f0b41786c8d4b4f506
sunckell <sunckell@gmail.com>
2016-02-08 13:29:31 -0500

    changed env file to env var.

--- skynet-twitter-aggregator.py
+++ skynet-twitter-aggregator.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from settings import EnvironmentSettings
 
@@ -11,7 +12,7 @@ root_folder_path = os.path.dirname(os.path.abspath(__file__))
 env_settings = EnvironmentSettings(root_folder_path)
 
 
-skynet_service = "http://52.90.10.34"
+skynet_service = "http://skynet.elasticbeanstalk.com/services/stream/publish/SkynetTwitterAggregator"
 
 
 # --- this is a basic listener that just prints received tweets to stdout.
@@ -25,13 +26,28 @@ class StdOutListener(StreamListener):
         print(status)
 
 
+class SkynetStreamPoster(StreamListener):
+
+    def on_data(self, data):
+        requests.post(skynet_service, data=json.dumps(data))
+        print("Posted data")
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
 if __name__ == '__main__':
 
     # --- This handles Twitter authentication and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(env_settings['CONSUMER_KEY'], env_settings['CONSUMER_SECRET'])
-    auth.set_access_token(env_settings['ACCESS_TOKEN'], env_settings['ACCESS_TOKEN_SECRET'])
-    stream = Stream(auth, l)
+    # --- l = StdOutListener()
+    p = SkynetStreamPoster()
+
+    auth = OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+    auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+
+    # --- stream = Stream(auth, l)
+    stream = Stream(auth, p)
 
     # --- This line filter Twitter Streams to capture data by the keywords.
     stream.filter(track=['NASDAQ:TROW', 'NASDAQ:MSFT', 'TROW', 'AAPL'])
